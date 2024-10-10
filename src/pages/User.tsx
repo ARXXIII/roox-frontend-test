64ed5256044d868dfc658d76ec484b83d87be6ca
Андрей Романов <human1488pwnz@yandex.ru>
2024-10-10 17:39:48 +0300

    feat: fields validation, edit & save functionality

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,6 +1,12 @@
-import { Input } from "../components/input"
+import clsx from "clsx"
+
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { AppDispatch, RootState } from "../store/store"
+import { fetchUserById, updateUser } from "../store/users-slice"
+
+import { Input } from "../components/input"
 import { Button } from "../components/button"
 
 type Geo = {
@@ -14,8 +20,8 @@ type Address = {
     city: string
     zipcode: string
     geo: Geo
-
 }
+
 type Company = {
     name: string
     catchPhrase: string
@@ -23,7 +29,7 @@ type Company = {
 }
 
 interface User {
-    id: string
+    id: number
     name: string
     username: string
     email: string
@@ -31,142 +37,215 @@ interface User {
     phone: string
     website: string
     company: Company
+    comment?: string
 }
 
-const User = () => {
+const UserPage = () => {
     const { id } = useParams<{ id: string }>()
+    const dispatch: AppDispatch = useDispatch()
 
-    const [isReadOnly, setIsReadOnly] = useState<boolean>(true)
-    const [name, setName] = useState<string>()
-    const [userName, setUserName] = useState<string>()
-    const [email, setEmail] = useState<string>()
-    const [street, setStreet] = useState<string>()
-    const [city, setCity] = useState<string>()
-    const [zipCode, setZipCode] = useState<string>()
-    const [phone, setPhone] = useState<string>()
-    const [website, setWebsite] = useState<string>()
-    const [comment, setComment] = useState<string>()
+    const user = useSelector((state: RootState) => state.users.users.find(u => u.id === Number(id)))
 
-    const fetchUser = async (id: string) => {
-        try {
-            const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const [name, setName] = useState<string>('')
+    const [username, setUsername] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+    const [street, setStreet] = useState<string>('')
+    const [city, setCity] = useState<string>('')
+    const [zipcode, setZipcode] = useState<string>('')
+    const [phone, setPhone] = useState<string>('')
+    const [website, setWebsite] = useState<string>('')
+    const [comment, setComment] = useState<string>('')
+    const [isDisabled, setIsDisabled] = useState<boolean>(true)
 
-            if (!response.ok) {
-                throw new Error('Failed to fetch user!')
+    const setStates = (source: User) => {
+        setName(source.name)
+        setUsername(source.username)
+        setEmail(source.email)
+        setStreet(source.address.street)
+        setCity(source.address.city)
+        setZipcode(source.address.zipcode)
+        setPhone(source.phone)
+        setWebsite(source.website)
+        setComment(source.comment!)
+    }
+
+    const [errors, setErrors] = useState({
+        name: false,
+        username: false,
+        email: false,
+        street: false,
+        city: false,
+        zipcode: false,
+        phone: false,
+        website: false,
+    })
+
+    const handleSave = () => {
+        if (validateForm() && user) {
+            const updatedUser = {
+                ...user,
+                name,
+                username,
+                email,
+                address: {
+                    street,
+                    city,
+                    zipcode,
+                    suite: user.address.suite,
+                    geo: {
+                        lat: user.address.geo.lat,
+                        lng: user.address.geo.lng
+                    }
+                },
+                phone,
+                website,
+                comment,
             }
 
-            const data = await response.json()
-
-            setName(data.name)
-            setUserName(data.username)
-            setEmail(data.email)
-            setStreet(data.address.street)
-            setCity(data.address.city)
-            setZipCode(data.address.zipcode)
-            setPhone(data.phone)
-            setWebsite(data.website)
-        } catch (error) {
-            console.error('Failed to fetch user!', error)
+            dispatch(updateUser(updatedUser))
+            setIsDisabled(true)
+            console.log('UPDATED USER', updatedUser)
         }
     }
 
-    useEffect(() => {
-        if (id) {
-            fetchUser(id)
+    const validateForm = () => {
+        const newErrors = {
+            name: !name,
+            username: !username,
+            email: !email,
+            street: !street,
+            city: !city,
+            zipcode: !zipcode,
+            phone: !phone,
+            website: !website,
         }
-    }, [id])
+
+        setErrors(newErrors)
+
+        return Object.values(newErrors).every(error => !error)
+    }
+
+    useEffect(() => {
+        if (!user) {
+            dispatch(fetchUserById(Number(id)))
+        } else {
+            setStates(user)
+        }
+    }, [dispatch, id, user])
 
 
     return (
         <article className="space-y-3 px-16 py-5">
             <div className="flex justify-between items-center">
                 <h1 className="font-bold text-sm">Профиль пользователя</h1>
-                <Button onClick={() => setIsReadOnly(false)}>Редактировать</Button>
+                <Button onClick={() => setIsDisabled(false)}>Редактировать</Button>
             </div>
-            <div className="space-y-2.5 px-3 py-5 border rounded-lg">
-                <Input
-                    id='name'
-                    label='Name'
-                    placeholder='Иван Иванов'
-                    autoComplete='cc-name'
-                    readOnly={isReadOnly}
-                    value={name!}
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <Input
-                    id="userName"
-                    label="User Name"
-                    placeholder="Ivan"
-                    autoComplete="username"
-                    readOnly={isReadOnly}
-                    value={userName!}
-                    onChange={(e) => setUserName(e.target.value)}
-                />
-                <Input
-                    type="email"
-                    id="email"
-                    label="E-mail"
-                    placeholder="example@mail.com"
-                    readOnly={isReadOnly}
-                    value={email!}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <Input
-                    id="street"
-                    label="Street"
-                    autoComplete="street-address"
-                    placeholder="ул. Пример"
-                    readOnly={isReadOnly}
-                    value={street!}
-                    onChange={(e) => setStreet(e.target.value)}
-                />
-                <Input
-                    id="city"
-                    label="City"
-                    placeholder="Москва"
-                    readOnly={isReadOnly}
-                    value={city!}
-                    onChange={(e) => setCity(e.target.value)}
-                />
-                <Input
-                    id="zipCode"
-                    label="Zip Code"
-                    autoComplete="postal-code"
-                    placeholder="1234234"
-                    readOnly={isReadOnly}
-                    value={zipCode!}
-                    onChange={(e) => setZipCode(e.target.value)}
-                />
-                <Input
-                    type="tel"
-                    id="phone"
-                    label="Phone"
-                    placeholder="89991112233"
-                    autoComplete="tel"
-                    readOnly={isReadOnly}
-                    value={phone!}
-                    onChange={(e) => setPhone(e.target.value)}
-                />
-                <Input
-                    id="website"
-                    label="Website"
-                    placeholder="www.example.com"
-                    readOnly={isReadOnly}
-                    value={website!}
-                    onChange={(e) => setWebsite(e.target.value)}
-                />
-                <div className="space-y-1">
-                    <label htmlFor="comment" className="block">Comment</label>
-                    <textarea
-                        id="comment"
-                        value={comment}
-                        onChange={(e) => setComment(e.target.value)}
-                        className="px-2.5 py-1 min-w-[420px] min-h-14 text-xs border rounded-md"
+            <form className="space-y-8 px-3 py-5 border rounded-lg">
+                <div className="space-y-2.5">
+                    <Input
+                        id='name'
+                        label='Name'
+                        placeholder='Иван Иванов'
+                        autoComplete='cc-name'
+                        disabled={isDisabled}
+                        error={errors.name}
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                     />
+                    <Input
+                        id="userName"
+                        label="User Name"
+                        placeholder="Ivan"
+                        autoComplete="username"
+                        disabled={isDisabled}
+                        error={errors.username}
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <Input
+                        type="email"
+                        id="email"
+                        label="E-mail"
+                        placeholder="example@mail.com"
+                        disabled={isDisabled}
+                        error={errors.email}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Input
+                        id="street"
+                        label="Street"
+                        autoComplete="street-address"
+                        placeholder="ул. Пример"
+                        disabled={isDisabled}
+                        error={errors.street}
+                        value={street}
+                        onChange={(e) => setStreet(e.target.value)}
+                    />
+                    <Input
+                        id="city"
+                        label="City"
+                        placeholder="Москва"
+                        disabled={isDisabled}
+                        error={errors.city}
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                    <Input
+                        id="zipCode"
+                        label="Zip Code"
+                        autoComplete="postal-code"
+                        placeholder="1234234"
+                        disabled={isDisabled}
+                        error={errors.zipcode}
+                        value={zipcode}
+                        onChange={(e) => setZipcode(e.target.value)}
+                    />
+                    <Input
+                        type="tel"
+                        id="phone"
+                        label="Phone"
+                        placeholder="89991112233"
+                        autoComplete="tel"
+                        disabled={isDisabled}
+                        error={errors.phone}
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                    />
+                    <Input
+                        id="website"
+                        label="Website"
+                        placeholder="www.example.com"
+                        disabled={isDisabled}
+                        error={errors.website}
+                        value={website}
+                        onChange={(e) => setWebsite(e.target.value)}
+                    />
+                    <div className="space-y-1">
+                        <label htmlFor="comment" className="block">Comment</label>
+                        <textarea
+                            id="comment"
+                            value={comment}
+                            disabled={isDisabled}
+                            onChange={(e) => setComment(e.target.value)}
+                            className={clsx("px-2.5 py-1 min-w-[420px] min-h-14 text-xs border rounded-md",
+                                isDisabled && 'text-neutral-200'
+                            )}
+                        />
+                    </div>
                 </div>
-            </div>
+                <div className="flex justify-end">
+                    <Button
+                        variant='send'
+                        disabled={isDisabled}
+                        onClick={handleSave}
+                    >
+                        Отправить
+                    </Button>
+                </div>
+            </form>
         </article>
     )
 }
 
-export default User
+export default UserPage
