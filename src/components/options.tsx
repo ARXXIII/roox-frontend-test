42635dc16b3027299a40378b93e42c77b0eb9748
Андрей Романов <human1488pwnz@yandex.ru>
2024-10-10 17:34:04 +0300

    Add sort functions & change bg-color

--- src/components/options.tsx
+++ src/components/options.tsx
@@ -1,14 +1,19 @@
+import { useDispatch } from "react-redux"
+import { sortByCity, sortByCompany } from "../store/users-slice"
+
 import { Button } from "./button"
 
 export const Option = () => {
+    const dispatch = useDispatch()
+
     return (
-        <aside className="space-y-3 h-full px-6 py-5 bg-neutral-500">
+        <aside className="space-y-3 h-full px-6 py-5 bg-[#BDBDBD]">
             <h1 className="text-xs">Сортировка</h1>
             <div className="flex flex-col gap-y-3">
-                <Button>
+                <Button onClick={() => dispatch(sortByCity())}>
                     По городу
                 </Button>
-                <Button>
+                <Button onClick={() => dispatch(sortByCompany())}>
                     По компании
                 </Button>
             </div>
