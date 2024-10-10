7c92dcdaa98d8c432d4c16696b4792063b451146
Андрей Романов <human1488pwnz@yandex.ru>
2024-10-10 17:34:35 +0300

    Add disabled state & error style

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,15 +1,18 @@
+import clsx from "clsx"
+
 type InputProps = {
     id: string
     label: string
     type?: string
     placeholder?: string
     autoComplete?: string
-    readOnly: boolean
+    disabled: boolean
+    error?: boolean
     value: string
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const Input = ({ id, label, type, placeholder, autoComplete, value, onChange, readOnly }: InputProps) => {
+export const Input = ({ id, label, type, placeholder, autoComplete, value, onChange, disabled, error }: InputProps) => {
     return (
         <div className="space-y-1 w-fit">
             <label htmlFor={id} className="block text-xs">{label}</label>
@@ -18,10 +21,13 @@ export const Input = ({ id, label, type, placeholder, autoComplete, value, onCha
                 id={id}
                 placeholder={placeholder}
                 autoComplete={autoComplete}
-                readOnly={readOnly}
                 value={value}
-                onChange={() => onChange}
-                className="px-2.5 py-1 min-w-52 text-xs border rounded-md"
+                onChange={onChange}
+                disabled={disabled}
+                className={clsx("px-2.5 py-1 min-w-52 text-xs border rounded-md",
+                    disabled && 'text-neutral-200',
+                    error && 'border-red-500'
+                )}
             />
         </div>
     )
